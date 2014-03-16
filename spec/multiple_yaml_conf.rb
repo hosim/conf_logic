a38c5e97277b8c91dcabfb0e13f0ca4616e77a79
hosim <github.hosim@gmail.com>
2014-03-16 11:53:20 +0900

    first commit

---
+++ spec/multiple_yaml_conf.rb
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+class MultipleYamlConf < ConfLogic::Container
+  conf_setting.source_dir "#{File.dirname(__FILE__)}/multiple_yaml"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+# coding: utf-8
+require 'rspec'
+require 'conf_logic'
+
+RSpec.configure do |config|
+end
