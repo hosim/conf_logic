f15ed8fe6f0ef304014c619bc92c59d8218a9cc9
hosim <github.hosim@gmail.com>
2014-03-19 08:37:58 +0900

    delete comments

--- lib/configue/setting.rb
+++ lib/configue/setting.rb
@@ -4,30 +4,28 @@ require "forwardable"
 
 module Configue
   class Setting
+    attr_reader :hash
+
     def initialize(owner_class)
       @owner_class = owner_class
     end
 
     def source_dir(dir)
       @hash ||= InnerHash.new
+
       Dir.glob("#{dir}/**/*.yml") do |filenm|
         h = YAML.load_file(filenm)
-#        @hash.__send__(:deep_update, h)
         @hash.deep_merge!(h)
-
-#        sig = class << @owner_class; self; end
-#        h.keys.each do |k|
-#          sig.__send__(:define_method, k, ->{ self[k] })
-#        end
       end
+
       methods = [:[], :key?, :has_key?] + @hash.keys
+
       @owner_class.extend SingleForwardable
       methods.each do |m|
         @owner_class.def_delegator "@setting.hash", m
       end
+
       @hash
     end
-
-    attr_reader :hash
   end
 end
