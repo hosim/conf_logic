5ed31a7da26f538d9f7f3c1a90fe58480752e2df
hosim <github.hosim@gmail.com>
2014-03-29 09:42:07 +0900

    add some comments

--- lib/configue/container.rb
+++ lib/configue/container.rb
@@ -3,8 +3,49 @@
 require "configue/criteria"
 
 module Configue
+  # +Configue::Container+ is a setting solution using a YAML file.
+  #
+  # When you have such configuration files:
+  #
+  #    # config/accounts/admin_users.yml
+  #    accounts:
+  #      admin_users:
+  #        - grumpy
+  #        - sneezy
+  #
+  #    # config/accounts/test_users.yml
+  #    accounts:
+  #      test_users:
+  #        - sleepy
+  #        - dopey
+  #
+  # this could be:
+  #
+  #    class Foo < Configue::Container
+  #      config.source_dir "#{File.dirname(__FILE__)}/config"
+  #    end
+  #
+  #    Foo.accounts.admin_users
+  #    # => ["grumpy", "sneezy"]
+  #
+  #    Foo.accounts.test_users
+  #    # => ["sleepy", "dopey"]
+  #
   class Container < Node
 
+    # When you do not know the setting has keys that you want to
+    # specify and want to avoid +NoMethodError+, you could use
+    # +query+ and +retrieve+.
+    #
+    #   Foo.query("accounts.admin_users").retrieve
+    #   # => ["grumpy", "sneezy"]
+    #
+    #   Foo.query("users.admin_users").retrieve
+    #   # => nil
+    #
+    #   Foo.query[:accounts][:admin_users].retrieve
+    #   # => ["grumpy", "sneezy"]
+    #
     def query(key=nil)
       q = Criteria.new(self)
       q = key.split('.').each.inject(q) {|c, k| c[k] } if key
@@ -12,6 +53,7 @@ module Configue
     end
 
     class << self
+      # +config+ allows you to access the object for setting container.
       def config
         @config_access_name = "config"
         @setting ||= Setting.new(self)
