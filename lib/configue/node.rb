e484d44d360584714311aa5dda26e17d60b94761
hosim <github.hosim@gmail.com>
2014-05-25 13:31:44 +0900

    separete a process of set up a node object

--- lib/configue/node.rb
+++ lib/configue/node.rb
@@ -8,29 +8,38 @@ module Configue
       raise TypeError unless hash.respond_to?(:[])
 
       node_type = hash.instance_variable_get(:@node_type) if hash.is_a? self.class
-      sig = class << self; self; end
       if hash.is_a? Hash or node_type == :hash
-        @hash = hash.each.inject({}) do |h, (k, v)|
-          sig.__send__(:define_method, k, ->{ self[k] })
-          h[k.to_s] = v; h
-        end
-        sig.__send__(:include, HashNode)
-        @node_type = :hash
+        setup_as_hash_node(hash)
       elsif hash.is_a? Array or node_type == :array
-        @hash = hash
-        sig.__send__(:include, ArrayNode)
-        @node_type = :array
+        setup_as_array_node(hash)
       end
       self
     end
 
     private
-    def node?(object)
+    def node?(object, counter=0)
       return true if object.is_a? Hash
-      if object.is_a? Array
-        return object.any? {|n| node?(n) }
-      end
+      return false if counter >= 3
+      return object.any? {|n| node?(n, counter + 1) } if object.is_a? Array
       return false
+    end
+
+    def setup_as_hash_node(hash)
+      sig = class << self; self; end
+      @hash = hash.each.inject({}) do |h, (k, v)|
+        sig.__send__(:define_method, k, ->{ self[k] })
+        h[k.to_s] = node?(v) ? self.class.new(v) : v; h
+      end
+      sig.__send__(:include, HashNode)
+      @node_type = :hash
+    end
+
+    def setup_as_array_node(array)
+      sig = class << self; self; end
+      @hash = array
+      @hash = array.map {|x| self.class.new(x) } if array.is_a? Array
+      sig.__send__(:include, ArrayNode)
+      @node_type = :array
     end
   end
 end
